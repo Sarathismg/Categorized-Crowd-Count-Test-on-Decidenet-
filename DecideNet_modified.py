be28f727bf6398a0f346fb09c11935e4b834c12d
Sarathismg <sarathismg@gmail.com>
2019-01-18 01:11:22 +0600

    Decidenet_modified CC Train

--- DecideNet_modified.py
+++ DecideNet_modified.py
@@ -4,7 +4,7 @@ import keras
 from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 from keras.layers.normalization import BatchNormalization
 from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Flatten, Input, Conv2DTranspose
+from keras.layers import Dense, Dropout, Flatten, Input, Conv2DTranspose, Add
 from keras.layers import Conv2D, MaxPooling2D, Reshape, Concatenate
 from keras.optimizers import Adam
 import numpy as np
@@ -27,8 +27,8 @@ train_path = './data/formatted_trainval/shanghaitech_part_' + dataset + '_patche
 train_den_path_sit = './data/formatted_trainval/shanghaitech_part_' + dataset + '_patches_9/train_den_sit/'
 train_den_path_stand = './data/formatted_trainval/shanghaitech_part_' + dataset + '_patches_9/train_den_stand/'
 
-detection_density_path = '/data/formatted_trainval/shanghaitech_part_A_patches_9/Alphapose_All'
-detection_density_path_mirrored = 'data/formatted_trainval/shanghaitech_part_A_patches_9/Alphapose_All_M'
+detection_density_path = './data/formatted_trainval/shanghaitech_part_A_patches_9/Alphapose_All/'
+detection_density_path_mirrored = './data/formatted_trainval/shanghaitech_part_A_patches_9/Alphapose_All_M/'
 
 val_path = './data/formatted_trainval/shanghaitech_part_' + dataset + '_patches_9/val/'
 val_den_path = './data/formatted_trainval/shanghaitech_part_' + dataset + '_patches_9/val_den/'
@@ -56,7 +56,7 @@ def data_pre_train():
 
     train_data = []
     for i in range(img_num):
-        if i % 100 == 0:
+        if i % 10 == 0:
             print(i, '/', img_num)
         name = train_img_names[i]
         # print(name + '****************************')
@@ -73,14 +73,15 @@ def data_pre_train():
         den_det_all_quarter = np.zeros((int(den_sit.shape[0] / 4), int(den_sit.shape[1] / 4)))
         den_det_all_mirrored_quarter = np.zeros((int(den_sit.shape[0] / 4), int(den_sit.shape[1] / 4)))
         # print(den_quarter.shape)
-        for i in range(len(den_quarter)):
+        '''for i in range(len(den_quarter)):
             for j in range(len(den_quarter[0])):
                 for p in range(4):
                     for q in range(4):
                         den_quarter[i][j] += den_sit[i * 4 + p][j * 4 + q] + den_stand[i * 4 + p][j * 4 + q]
                         den_det_all_quarter += den_detection_all[i * 4 + p][j * 4 + q]
-                        den_det_all_mirrored_quarter += den_detection_all_mirrored[i * 4 + p][j * 4 + q]
-        train_data.append([img, den_quarter, den_det_all_quarter, den_det_all_mirrored_quarter])
+                        den_det_all_mirrored_quarter += den_detection_all_mirrored[i * 4 + p][j * 4 + q]'''
+        #train_data.append([img, den_quarter, den_det_all_quarter, den_det_all_mirrored_quarter])
+        train_data.append([img, den_sit+den_stand , den_detection_all, den_detection_all_mirrored])
         #plt.imshow(img)
         #plt.imshow(den_sit+den_stand,alpha=0.75)
         #plt.show()
@@ -139,6 +140,9 @@ y_train = np.array(y_train)
 x_detection_den = np.array(x_detection_den)
 x_detection_den_mirror = np.array(x_detection_den_mirror)
 
+plt.imshow(x_train[0].reshape(256,256))
+plt.imshow(x_detection_den[0].reshape(256,256), alpha=0.7)
+plt.show()
 
 x_detection_den = np.append(x_detection_den, x_detection_den_mirror, axis=0)
 
@@ -167,48 +171,34 @@ def mssse(y_true, y_pred):
 def customLoss(y_true, y_pred):
     loss1 = mssse(y_true, y_pred)
     loss2 = K.mean((y_true - y_pred)**2)
-    return 0.7*loss1 + 0.3*loss2
+    return 0.7*loss2 + 0.3*loss1
 
-inputs = Input(shape=(None, None, 1))
+inputs_1 = Input(shape=(None, None, 1))
+inputs_2 = Input(shape=(None, None, 1))
 
-conv_s = Conv2D(24, (5, 5), padding='same', activation='relu')(inputs)
-conv_s = MaxPooling2D(pool_size=(2, 2))(conv_s)
-conv_s = (conv_s)
-conv_s = Conv2D(48, (3, 3), padding='same', activation='relu')(conv_s)
-conv_s = MaxPooling2D(pool_size=(2, 2))(conv_s)
-conv_s = Conv2D(24, (3, 3), padding='same', activation='relu')(conv_s)
-conv_s = Conv2D(12, (3, 3), padding='same', activation='relu')(conv_s)
-# conv_s = Conv2D(1, (1, 1), padding = 'same', activation = 'relu')(conv_s)
+conv_s = Conv2D(20, (7, 7), padding='same', activation='relu')(inputs_1)
+conv_s = Conv2D(40, (5, 5), padding='same', activation='relu')(conv_s)
+conv_s = Conv2D(20, (5, 5), padding='same', activation='relu')(conv_s)
+conv_s = Conv2D(10, (5, 5), padding='same', activation='relu')(conv_s)
+conv_regnet = Conv2D(1, (1, 1), padding='same')(conv_s)
 
-conv_m = Conv2D(20, (7, 7), padding='same', activation='relu')(inputs)
-conv_m = MaxPooling2D(pool_size=(2, 2))(conv_m)
-conv_m = (conv_m)
-conv_m = Conv2D(40, (5, 5), padding='same', activation='relu')(conv_m)
-conv_m = MaxPooling2D(pool_size=(2, 2))(conv_m)
-conv_m = Conv2D(20, (5, 5), padding='same', activation='relu')(conv_m)
-conv_m = Conv2D(10, (5, 5), padding='same', activation='relu')(conv_m)
-# conv_m = Conv2D(1, (1, 1), padding = 'same', activation = 'relu')(conv_m)
+conv_denet = inputs_2
 
+conv_stacked = Concatenate(axis=3)([inputs_1, conv_denet, conv_regnet])
 
-conv_l = Conv2D(16, (9, 9), padding='same', activation='relu')(inputs)
-conv_l = MaxPooling2D(pool_size=(2, 2))(conv_l)
-conv_l = (conv_l)
-conv_l = Conv2D(32, (7, 7), padding='same', activation='relu')(conv_l)
-conv_l = MaxPooling2D(pool_size=(2, 2))(conv_l)
-conv_l = Conv2D(16, (7, 7), padding='same', activation='relu')(conv_l)
-conv_l = Conv2D(8, (7, 7), padding='same', activation='relu')(conv_l)
-# conv_l = Conv2D(1, (1, 1), padding = 'same', activation = 'relu')(conv_l)
+conv_qua = Conv2D(20, (7, 7), padding='same', activation='relu')(conv_stacked)
+conv_qua = Conv2D(40, (5, 5), padding='same', activation='relu')(conv_qua)
+conv_qua = Conv2D(20, (5, 5), padding='same', activation='relu')(conv_qua)
+conv_qua = Conv2D(1, (1,1), padding='same', activation='relu')(conv_qua)
 
-conv_concat3 = Concatenate(axis=3)([conv_m, conv_s, conv_l])
-#result = Conv2D(1, (1, 1), padding='same')(conv_merge)
-result = Conv2D(1, (1, 1), padding='same')(conv_concat3)
+conv_final = Add()([conv_regnet, conv_qua, conv_denet])
 
 
-model = Model(inputs=inputs, outputs=result)
+model = Model(inputs=[inputs_1, inputs_2], outputs=conv_final)
 model.summary()
 reduce_lr = ReduceLROnPlateau(monitor='val_maaae', factor=0.90, cooldown=10,
                               patience=10, min_lr=1e-5)
-kb = keras.callbacks.TensorBoard(log_dir='./logs/mcnn_zhang_aes', histogram_freq=0, batch_size=32, write_graph=True, write_grads=False, write_images=False, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None, embeddings_data=None, update_freq='epoch')
+kb = keras.callbacks.TensorBoard(log_dir='./logs/Decidenet_modified', histogram_freq=0, batch_size=32, write_graph=True, write_grads=False, write_images=False, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None, embeddings_data=None, update_freq='epoch')
 
 callbacks_list = [reduce_lr, kb]
 adam = Adam(lr=5e-3)
@@ -226,15 +216,17 @@ x_train = np.append(x_train, np.flip(x_train, axis=2), axis=0)
 y_train = np.append(y_train, np.flip(y_train, axis=2), axis=0)
 
 
+
+
 print(y_train.shape)
 
-'''plt.imshow(x_train[0].reshape(256,256))
-plt.imshow(y_train[0].reshape(64,64), alpha=0.7)
+plt.imshow(x_train[0].reshape(256,256))
+plt.imshow(y_train[0].reshape(256,256), alpha=0.7)
 plt.show()
 
 plt.imshow(x_train[ll].reshape(256,256))
-plt.imshow(y_train[ll].reshape(64,64), alpha=0.7)
-plt.show()'''
+plt.imshow(y_train[ll].reshape(256,256), alpha=0.7)
+plt.show()
 
 
 
@@ -242,21 +234,5 @@ list_ = np.arange(len(x_train))
 np.random.shuffle(list_)
 
 for i in range(1):
-    model.fit(x_train[list_[0:int(0.8*len(list_))]], y_train[list_[0:int(0.8*len(list_))]], epochs=2000, batch_size=32, callbacks=callbacks_list, validation_data=(x_train[list_[int(0.8*len(list_)): ]], y_train[list_[int(0.8*len(list_)):]]))
+    model.fit([x_train[list_[0:int(0.8*len(list_))]], x_detection_den[list_[0:int(0.8*len(list_))]]], y_train[list_[0:int(0.8*len(list_))]], epochs=2000, batch_size=32, callbacks=callbacks_list, validation_data=([x_train[list_[int(0.8*len(list_)): ]], x_detection_den[list_[int(0.8*len(list_)): ]]], y_train[list_[int(0.8*len(list_)):]]))
     model.save('models/mcnn_zhang.h5')
-    '''score = model.evaluate(x_test[:], y_test[:], batch_size=16)
-    score[2] = math.sqrt(score[2])
-    print(score)
-    if score[1] < best_mae:
-        best_mae = score[1]
-        best_mae_mse = score[2]
-
-        json_string = model.to_json()
-        open('model.json', 'w').write(json_string)
-        model.save_weights('weights.h5')
-    if score[2] < best_mse:
-        best_mse = score[2]
-        best_mse_mae = score[1]
-
-    print('best mae: ', best_mae, '(', best_mae_mse, ')')
-    print('best mse: ', '(', best_mse_mae, ')', best_mse)'''
